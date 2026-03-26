ec7ad86f47e377814c7ff82a2e92deecae54d159
KimAngelohmm <kimangelobadic10@gmail.com>
2026-03-26 21:50:50 +0800

    First Upload (10%)

---
+++ scenes/load_game4.gd
@@ -0,0 +1,47 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_new_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/new_game.tscn")
+
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_button_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/load_game1.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/load_game2.tscn")
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/load_game3.tscn")
+
+
+func _on_button_5_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/load_game5.tscn")
+
+
+func _on_button_6_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/load_game6.tscn")
+
+
+func _on_return_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
